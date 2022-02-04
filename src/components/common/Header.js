bbc502a4e5880b166c5d258aaba0993dc15b2c92
RamK777-stack <ramk97095@gmail.com>
2022-02-04 21:40:49 +0530

    user profile pic

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,6 +3,7 @@ import {Fragment} from "react"
 import {Disclosure, Menu, Transition} from "@headlessui/react"
 import {BellIcon, MenuIcon, XIcon} from "@heroicons/react/outline"
 import Logo from "../../assets/images/feather.svg"
+import user from "../../assets/images/user.png"
 import ThemeToggle from "./ThemeToggle"
 import {Link} from "react-router-dom"
 import {URL_PATH} from "../../utils/urlPath"
@@ -61,7 +62,7 @@ const Header = () => {
                           <span className="sr-only">Open user menu</span>
                           <img
                             className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            src={user}
                             alt="user profile"
                           />
                         </Menu.Button>
