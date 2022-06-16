de52188a31e48a8eb3cd1c2adc66989274e7703a
RamK777-stack <ramk97095@gmail.com>
2022-06-16 17:59:06 +0530

    Added next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://writeonce.dev',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['localhost', 'res.cloudinary.com'],
   },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     require('./scripts/generate-sitemap')
+  //   }
+
+  //   return config
+  // },
 }
